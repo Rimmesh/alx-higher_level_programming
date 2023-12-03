5b143c68090e6cde8378a365365cc4d98e553e3a
Rimmesh <rimmeshete@gmail.com>
2023-12-03 09:30:41 -0800

    lists

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,71 +1,60 @@
 #include "lists.h"
-
 /**
  * reverse_listint - reverses a linked list
  * @head: pointer to first
- *
  * Return: pointer of first node
  */
 void reverse_listint(listint_t **head)
 {
-  listint_t *prev = NULL;
-  listint_t *current = *head;
-  listint_t *next = NULL;
-
-  while (current)
-    {
-      next = current->next;
-      current->next = prev;
-      prev = current;
-      current = next;
-    }
-
-  *head = prev;
+listint_t *prev = NULL;
+listint_t *current = *head;
+listint_t *next = NULL;
+while (current)
+{
+next = current->next;
+current->next = prev;
+prev = current;
+current = next;
+}
+*head = prev;
 }
 /**
  * is_palindrome - sees if linked list is palindrome
  * @head: double pointer to linked list
- *
  * Return: 1 if palindrome,otherwise 0
  */
 int is_palindrome(listint_t **head)
 {
-  listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
-
-  if (*head == NULL || (*head)->next == NULL)
-    return (1);
-
-  while (1)
-    {
-      fast = fast->next->next;
-      if (!fast)
-	{
-	  dup = slow->next;
-	  break;
-	}
-      if (!fast->next)
-	{
-	  dup = slow->next->next;
-	  break;
-	}
-      slow = slow->next;
-    }
-
-  reverse_listint(&dup);
-
-  while (dup && temp)
-    {
-      if (temp->n == dup->n)
-	{
-	  dup = dup->next;
-	  temp = temp->next;
-	}
-      else
-	return (0);
-    }
-
-  if (!dup)
-    return (1);
-
-  return (0);
+listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+if (*head == NULL || (*head)->next == NULL)
+return (1);
+while (1)
+{
+fast = fast->next->next;
+if (!fast)
+{
+dup = slow->next;
+break;
+}
+if (!fast->next)
+{
+dup = slow->next->next;
+break;
+}
+slow = slow->next;
+}
+reverse_listint(&dup);
+while (dup && temp)
+{
+if (temp->n == dup->n)
+{
+dup = dup->next;
+temp = temp->next;
+}
+else
+return (0);
+}
+if (!dup)
+return (1);
+return (0);
 }
